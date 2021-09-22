1bc5aadd152b35f8bd3bafa1bdec2af9e06b4f5f
gramkrishna <reachme.rk@gmail.com>
2021-09-22 20:39:52 +0530

    Files for user authentication on client side.

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,16 +1,22 @@
-export default (posts=[], action) => {
+import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKE} from '../constants/actionTypes';
+const postsReducer = (posts=[], action) => {
     switch (action.type) {
-        case 'UPDATE':
+        case UPDATE:
+        case LIKE :
             return posts.map((post) => post._id === action.payload._id ? action.payload: post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             console.log("Fetch reducers called");
             return action.payload ;
-        case 'CREATE':
-            return [... posts, action.payload] ;
-        case `DELETE`:
+        case CREATE:
+            return [...posts, action.payload] ;
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-            default:
+       //case AUTH:
+         //   console.log("Auth is called here");
+        default:
             return posts;
 
     }
-}
+};
+
+export default postsReducer ;
